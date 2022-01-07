8176f6ef05cab24214b7d2a5ba27926efe022c7f
Parisa Salmani <55203321+parisasl@users.noreply.github.com>
2022-01-07 18:20:13 +0330

    Update TwentyNewsgroupsDataSet.py

--- TwentyNewsgroupsDataSet.py
+++ TwentyNewsgroupsDataSet.py
@@ -1,7 +1,7 @@
 
 # Objective:
 
-# The 20 Newsgroups data set is a collection of A number of newspapers group documents,
+# The 20 Newsgroups data set is a collection of a number of newspapers group documents,
 # partitioned (nearly) evenly across 20 different newsgroups. To the best of my knowledge,
 # it was originally collected by Ken Lang, probably for his Newsweeder: Learning to filter netnews paper, 
 # though he does not explicitly mention this collection. 
@@ -36,10 +36,7 @@
 # ###### alt
 # - atheism
 
-# ## Environment Setup & Dataset Loading:
-
-# In[1]:
-
+# Environment Setup & Dataset Loading:
 
 from sklearn.datasets import fetch_20newsgroups
 from sklearn.feature_extraction.text import TfidfVectorizer
@@ -51,16 +48,11 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 
-# In[2]:
-
-
 newsgroups_train = fetch_20newsgroups(subset='train')
 newsgroups_test = fetch_20newsgroups(subset='test')
 
 
-# ##  Preparing & Preprocessing the Data:
-
-# In[3]:
+# Preparing & Preprocessing the Data:
 
 
 vectorizer = TfidfVectorizer() #Convert a collection of raw documents to a matrix of TF-IDF features. 
@@ -70,25 +62,17 @@ vectors_test = vectorizer.transform(newsgroups_test.data)
 print(vectors_train.shape)
 
 
-# ## Visualizing the Data
-
-# In[4]:
+# Visualizing the Data
 
 
 print('Training data size:', len(newsgroups_train['data']))
 print('Test data size:', len(newsgroups_test['data']))
 
 
-# In[5]:
-
-
 #to return the number of appearance (frequency) of each news group
 targets, frequency = np.unique(newsgroups_train.target, return_counts=True)
 targets_str = np.array(newsgroups_train.target_names)
 print(list(zip(targets_str, frequency)))
-
-
-# In[6]:
 
 
 fig=plt.figure(figsize=(10, 5), dpi= 100)
@@ -99,22 +83,12 @@ plt.xlabel('News Group')
 plt.ylabel('Frequency')
 plt.show()
 
-
-# In[7]:
-
-
 pd.DataFrame({'data': newsgroups_train.data, 'target': newsgroups_train.target})
-
-
-# In[8]:
 
 
 targets_test, frequency_test = np.unique(newsgroups_test.target, return_counts=True)
 targets_test_str = np.array(newsgroups_test.target_names)
 print(list(zip(targets_test_str, frequency_test)))
-
-
-# In[9]:
 
 
 fig=plt.figure(figsize=(10, 5), dpi= 100)
@@ -126,15 +100,10 @@ plt.ylabel('Frequency')
 plt.show()
 
 
-# In[10]:
-
-
 pd.DataFrame({'data': newsgroups_test.data, 'target': newsgroups_test.target})
 
 
-# ### Building & Training Model:
-
-# In[11]:
+# Building & Training Model:
 
 
 #train the model
@@ -142,17 +111,12 @@ clf = MultinomialNB(alpha=0.01)
 clf.fit(vectors_train, newsgroups_train.target)
 
 
-# ### Evaluating Model:
-
-# In[12]:
+# Evaluating Model:
 
 
 # predict the group on test set
 pred = clf.predict(vectors_test)
 pd.DataFrame({"real value": newsgroups_test.target, "Predicted value": pred})
-
-
-# In[13]:
 
 
 #accuracy 
